2f9f6817261892e52a4e28d7188089d57083dd07
Torben Rydiander <torben@rydiander.dk>
2017-07-03 20:12:20 +0200

    A bit nicer

--- AntalDage.py
+++ AntalDage.py
@@ -52,9 +52,21 @@ def read_db(conn):
    		count = count + 1
 	return lineno, beg, dat, count
 
+def opret_dato():
+	print("Opret dato")
+
+def slet_dato():
+	print("Slet dato")
+
 def close_db(conn):
 	conn.close()
 	#print("Database closed.")
+
+def print_all(lineno, dat, beg, today):
+	for x in range(0, count):
+		dato = datetime.datetime.strptime(dat[x], "%Y-%m-%d").date()
+		count_days = num_days(today,dato)
+		print(lineno[x],"Antal dage", count_days, "til", beg[x], ",", dato, "som er en",is_day(dato))
 
 conn = open_db()
 import datetime
@@ -70,13 +82,18 @@ lineno, beg, dat, count = read_db(conn)
 #print(lineno)
 #print(beg)
 #print(dat)
-for x in range(0, count):
-	dato = datetime.datetime.strptime(dat[x], "%Y-%m-%d").date()
-	count_days = num_days(today,dato)
-	print("Antal dage", count_days, "til", beg[x], ",", dato, "som er en",is_day(dato))
-	
+print("1. Læs alle datoer")
+print("2. Opret en dato")
+print("3. Slet en dato")
+answer = input("Vælg:")
+if answer == "1":
+	print_all(lineno, dat, beg, today)
+if answer == "2":
+	opret_dato()
+if answer == "3":
+	slet_dato()
 #print((dato-today).days)
-print(count)
+#print(count)
 close_db(conn)
 
 #print("Operation done successfully, bye, bye.")
